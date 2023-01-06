1ee7a4621104619e22aa021a0fc119132410c5ef
shyler07 <113320798+shyler07@users.noreply.github.com>
2023-01-06 09:24:46 +0800

    Add files via upload

--- dist/js/tracer.js
+++ dist/js/tracer.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
     let convert_Btn = document.querySelector("#convert-btn");
     let reset_Btn = document.querySelector("#reset-btn");
 
-    let name_Text = document.querySelector("#name-text");
+    let fname_Text = document.querySelector("#fname-text");
+    let lname_Text = document.querySelector("#lname-text");
     let result_List = document.querySelector("#result-list");
 
     let font_Label = document.querySelector("#font-label");
@@ -23,18 +24,20 @@ document.addEventListener("DOMContentLoaded", ()=> {
             // create a element 
              let para_element = document.createElement('li');
             // create text in a element wih the value of input field/class
-            let text_element = document.createTextNode(name_Text.value);
-
+            let text_element1 = document.createTextNode(fname_Text.value);
+            let text_element2 = document.createTextNode(lname_Text.value);
             //console.log("Convert Clicked");
 
              // css inline css in javascript
             para_element.style.fontFamily = "Codystar"  
-            para_element.style.fontSize = "50px" 
-
+            para_element.style.fontSize = "70px" 
+            
             // create a element -> createtextelement/node
-            para_element.appendChild(text_element);
+            para_element.appendChild(text_element1);
+            para_element.appendChild(text_element2);
             //get id of the element -> create element with value
             result_List.appendChild(para_element);
+            
           }
    
        
@@ -46,14 +49,16 @@ document.addEventListener("DOMContentLoaded", ()=> {
             // create a element 
             let para_element = document.createElement('li');
             // create text in a element wih the value of input field/class
-            let text_element = document.createTextNode(name_Text.value);
+            let text_element1 = document.createTextNode(fname_Text.value);
+            let text_element2 = document.createTextNode(lname_Text.value);
  
             // css inline css in javascript
             para_element.style.fontFamily = "Raleway Dots"   
-            para_element.style.fontSize = "50px" 
+            para_element.style.fontSize = "70px" 
             //console.log("Convert Clicked");
         
-             para_element.appendChild(text_element);
+             para_element.appendChild(text_element1);
+             para_element.appendChild(text_element2);
             result_List.appendChild(para_element);
 
         }      
@@ -64,7 +69,9 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
     // clear button
     reset_Btn.addEventListener("click", () => {
-        document.querySelector('#name-text').value= "";
+        
+        document.querySelector('#fname-text').value= "";
+        document.querySelector('#lname-text').value= "";
        
         // remove element
         document.querySelector('ul').innerHTML="";
